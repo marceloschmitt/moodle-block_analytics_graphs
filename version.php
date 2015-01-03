4dbbb5857eba3e66b7235133617cc2d68515b001
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2015-01-02 22:26:00 -0200

    Release 1.1 dev

--- version.php
+++ version.php
@@ -1,4 +1,4 @@
 <?php
-$plugin->version = 2014122901;  // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->version = 2015010200;  // YYYYMMDDHH (year, month, day, 24-hr time)
 $plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
 
