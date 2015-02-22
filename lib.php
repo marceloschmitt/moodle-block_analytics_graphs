6288f67a669bff5b9c28f1e6f9852c7d3620c581
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2015-02-22 15:34:18 -0300

    Eliminate suspended users from graphs

--- lib.php
+++ lib.php
@@ -35,8 +35,9 @@ function block_analytics_graphs_subtract_student_arrays($estudantes, $acessaram)
 
 function block_analytics_graphs_get_students($course) {
     $context = get_context_instance(CONTEXT_COURSE, $course);
-    $estudantes = get_role_users(array(5), $context, false, '', 'firstname');
-    return($estudantes);
+    $students = get_role_users(array(5), $context, false, '', 'firstname', null, '', '', '', 'u.suspended = :xsuspended',
+                                                                                                    array('xsuspended'=>0));
+    return($students);
 }
 
 
