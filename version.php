c125965d9cfd2965eb1629088ca7846cefdc2c6c
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2015-03-20 11:07:44 -0300

    Issues #7 and #8

--- version.php
+++ version.php
@@ -16,8 +16,8 @@
 
 
 
-$plugin->version = 2015031601;  // YYYYMMDDHH (year, month, day, 24-hr time).
+$plugin->version = 2015032001;  // YYYYMMDDHH (year, month, day, 24-hr time).
 $plugin->requires = 2011120500; // YYYYMMDDHH (This is the release version for Moodle 2.0).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = 'v1.1';
+$plugin->release = 'v1.1.2';
 $plugin->component = 'block_analytics_graphs';
