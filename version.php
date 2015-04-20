9c515ac2741262b00581a4c15edbcf55ae1fc9cc
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2015-04-20 09:58:48 -0300

    Database creation
    The tables to keep messagens for future report were created.

--- version.php
+++ version.php
@@ -16,8 +16,8 @@
 
 
 
-$plugin->version = 2015032401;  // YYYYMMDDHH (year, month, day, 24-hr time).
+$plugin->version = 2015042001;  // YYYYMMDDHH (year, month, day, 24-hr time).
 $plugin->requires = 2011120500; // YYYYMMDDHH (This is the release version for Moodle 2.0).
 $plugin->maturity = MATURITY_STABLE; 
-$plugin->release = 'v1.1.2';
+$plugin->release = 'v1.2.0';
 $plugin->component = 'block_analytics_graphs';
