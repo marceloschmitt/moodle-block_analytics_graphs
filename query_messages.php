f8d03a540825b164e1d168572acbfc999011ad5c
Thiago Rodrigues <trodrigues02@gmail.com>
2015-05-15 15:34:28 -0300

    Fix access of object property

--- query_messages.php
+++ query_messages.php
@@ -43,7 +43,7 @@ $result = $DB->get_records_sql($sql, $params);
 if (count($result) > 0) {
 	$keys = array_keys($result);
 	for($x=0; $x<count($keys); $x++){
-		$result[$keys[$x]]["timecreated"] = usergetdate($result[$keys[$x]]["timecreated"]);
+		$result[$keys[$x]]->timecreated = usergetdate($result[$keys[$x]]->timecreated);
 	}
     echo json_encode($result);
 } else {
