074523b20db88dc15dd179e72a26ca00feaf4fc3
Thiago Rodrigues <trodrigues02@gmail.com>
2015-05-26 17:43:57 -0300

    Resources access pie chart on student info tab

--- query_resources_access.php
+++ query_resources_access.php
@@ -1,11 +1,21 @@
 <?php
-require('../../config.php');
+require_once('../../config.php');
 include("lib.php");
+require_once($CFG->dirroot.'/lib/moodlelib.php');
 
 $studentid = required_param('student_id', PARAM_INT);
 $courseid = required_param('course_id', PARAM_INT);
 
+/* Access control */
+require_login($courseid);
+$context = context_course::instance($courseid);
+require_capability('block/analytics_graphs:viewpages', $context);
+
 $result =  block_analytics_graphs_get_user_resource_url_page_access($courseid, $studentid, 0);
 
-echo json_encode($result);
+if (count($result) > 0) {
+    echo json_encode($result);
+} else {
+    echo json_encode(array());
+}
 ?>
