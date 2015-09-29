f20f28b5e5451b1ec1f1989ee5e4eb2c2e877e86
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2015-09-29 10:12:59 -0300

    Remove header - needs more testing

--- grades_chart.php
+++ grades_chart.php
@@ -27,11 +27,12 @@ require_login($courseid);
 $context = context_course::instance($courseid);
 require_capability('block/analytics_graphs:viewpages', $context);
 
-
+/*
 $PAGE->set_url(new moodle_url('/blocks/analytics_graphs/grades_chart.php', array('id' => $courseid)));
 $PAGE->set_context(context_course::instance($courseid));
 $PAGE->set_pagelayout('print');
 echo $OUTPUT->header();
+*/
 
 $sql = "SELECT gi.id, categoryid, fullname, itemname, gradetype, grademax, grademin
 			FROM {grade_categories} gc
