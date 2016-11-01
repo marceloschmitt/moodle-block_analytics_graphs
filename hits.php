4465f1290813ec470ac2185affb3dcdd3c7074a9
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2016-11-01 17:17:23 -0200

    Update hits.php
    Include header

--- hits.php
+++ hits.php
@@ -27,6 +27,11 @@ require_login($course);
 $context = context_course::instance($course);
 require_capability('block/analytics_graphs:viewpages', $context);
 
+$PAGE->set_url(new moodle_url('/blocks/analytics_graphs/hits.php', array('id' => $course, 'legacy' => 0)));
+$PAGE->set_context(context_course::instance($course));
+$PAGE->set_pagelayout('print');
+echo $OUTPUT->header();
+
 $courseparams = get_course($course);
 $startdate = $courseparams->startdate;
 $coursename = get_string('course', 'block_analytics_graphs') . ": " . $courseparams->fullname;
