9c16ccdd5e6c07f654898639a3cd3445b55bffca
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2017-01-15 15:39:05 -0200

    Fix version number

--- version.php
+++ version.php
@@ -16,7 +16,7 @@
 
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2017010501;  // YYYYMMDDHH (year, month, day, 24-hr time).
+$plugin->version = 2017011501;  // YYYYMMDDHH (year, month, day, 24-hr time).
 $plugin->requires = 2015111600; // YYYYMMDDHH (This is the release version for Moodle 2.9).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v3.2.1';
