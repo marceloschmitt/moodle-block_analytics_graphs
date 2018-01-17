616c61f84be3a238366f1940c9b01ecd41391095
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2018-01-17 15:01:11 -0200

    Update upgrade.php

--- db/upgrade.php
+++ db/upgrade.php
@@ -84,5 +84,5 @@ function xmldb_block_analytics_graphs_upgrade($oldversion, $block) {
         }
     }
     // Analytics_graphs savepoint reached.
-    upgrade_block_savepoint(true, 2017111601, 'analytics_graphs');
+    upgrade_block_savepoint(true, 2018011701, 'analytics_graphs');
 }
