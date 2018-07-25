26ee4f20274373c7cdd1dc302e40933cd3d316aa
Marcelo Augusto Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2018-07-25 11:19:40 -0300

    Update version

--- version.php
+++ version.php
@@ -16,7 +16,7 @@
 
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2018072512;  // YYYYMMDDHH (year, month, day, 24-hr time).
+$plugin->version = 2018072501;  // YYYYMMDDHH (year, month, day, 24-hr time).
 $plugin->requires = 2015111600; // YYYYMMDDHH (This is the release version for Moodle 3.0).
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = 'v4.1.1';
