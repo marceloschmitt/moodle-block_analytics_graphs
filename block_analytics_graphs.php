a83d8cf7cfacd62c9ac6b12e5ea3b072ae9db8f7
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2022-06-28 14:39:28 -0300

    Update block_analytics_graphs.php

--- block_analytics_graphs.php
+++ block_analytics_graphs.php
@@ -13,7 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-defined('MOODLE_INTERNAL') || die();
+
 class block_analytics_graphs extends block_base {
     public function init() {
         $this->title = get_string('analytics_graphs', 'block_analytics_graphs');
