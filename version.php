f1798bbe5e5a8f947c480f83223402cb52c2077d
Marcelo A. Rauh Schmitt <marcelo.schmitt@poa.ifrs.edu.br>
2026-09-21 15:43:28 -0300

    Update version.php

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version = 2026052803;  // YYYYMMDDHH (year, month, day, 24-hr time).
+$plugin->version = 2026092100;  // YYYYMMDDHH (year, month, day, 24-hr time).
 $plugin->requires = 2015111600; // YYYYMMDDHH (This is the release version for Moodle 3.0).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '4.2.4';
-$plugin->branch = '4.2';
+$plugin->release = '4.2.1';
+$plugin->branch = '4.3';
 $plugin->component = 'block_analytics_graphs';
